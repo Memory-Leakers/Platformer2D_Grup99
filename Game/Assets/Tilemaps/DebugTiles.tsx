e9ae343141c2e929215cc350c4bca3f631ef73de
MrIllas <44169046+MrIllas@users.noreply.github.com>
2021-10-25 00:03:16 +0200

    Map Level 1, Debug Tiles

---
+++ Game/Assets/Tilemaps/DebugTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="DebugTiles" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="../Terrain/DebugTiles.png" width="80" height="80"/>
+</tileset>
---
+++ Game/Assets/Tilemaps/TilemapTerrain.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="TilemapTerrain" tilewidth="16" tileheight="16" tilecount="242" columns="22">
+ <image source="../Terrain/Terrain (16x16).png" width="352" height="176"/>
+ <wangsets>
+  <wangset name="Terrains" type="mixed" tile="7">
+   <wangcolor name="Grass" color="#ff0000" tile="7" probability="1"/>
+   <wangcolor name="Wood" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="28" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="29" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="30" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="50" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="51" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="52" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="112" wangid="0,0,0,0,2,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
