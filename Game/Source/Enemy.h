0a99792525e650db5abaafb8fb19d748e2bbbe6e
Loproxi <79161178+Loproxi@users.noreply.github.com>
2021-11-15 21:01:32 +0100

    Enemy base class added

---
+++ Game/Source/Enemy.h
@@ -0,0 +1,27 @@
+#include "Collider.h"
+
+#pragma once
+class Enemy
+{
+public:
+	Enemy();
+
+	~Enemy();
+
+	virtual bool Start();
+
+	// Called each loop iteration
+	virtual bool PreUpdate();
+
+	// Called each loop iteration
+	virtual bool Update(float dt);
+
+	// Called each loop iteration
+	virtual bool PostUpdate();
+
+	// Called before quitting
+	virtual bool CleanUp();
+
+	virtual void OnCollision(Collider* col);
+};
+
